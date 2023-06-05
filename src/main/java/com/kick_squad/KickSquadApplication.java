a7f4583dc25b7ea438e4da8b420167e45217ff0b
nicolas <nicolas@LAPTOP-EG2DO9AJ>
2023-06-04 20:22:13 -0600

    com1_ks

---
+++ src/main/java/com/kick_squad/KickSquadApplication.java
@@ -0,0 +1,13 @@
+package com.kick_squad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KickSquadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KickSquadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kick_squad/KickSquadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kick_squad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KickSquadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
